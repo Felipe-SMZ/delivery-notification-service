849d7442911d67a80add4a767185f188c91bcb30
Felipe <felipeshimizu5@gmail.com>
2026-05-18 20:39:41 -0300

    first commit

---
+++ src/main/java/com/felipeshimizu/deliverynotificationservice/DeliveryNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.felipeshimizu.deliverynotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryNotificationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/felipeshimizu/deliverynotificationservice/DeliveryNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipeshimizu.deliverynotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryNotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
